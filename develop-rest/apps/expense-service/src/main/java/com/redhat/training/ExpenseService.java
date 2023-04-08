9733ac4e7aa14500756bc5358dfca0180a6c8ef8
zeeshan <shani.pu@gmail.com>
2023-04-08 17:27:26 +0300

    evelop-rest

--- develop-rest/apps/expense-service/src/main/java/com/redhat/training/ExpenseService.java
+++ develop-rest/apps/expense-service/src/main/java/com/redhat/training/ExpenseService.java
@@ -1,16 +1,25 @@
 package com.redhat.training;
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class ExpenseService {
-    private Set<Expense> expenses = Collections.newSetFromMap(Collections.synchronizedMap(new HashMap<>()));
 
+    @PostConstruct
+    void init(){
+        expenses.add(new Expense("Quarkus for Spring Developers",
+        Expense.PaymentMethod.DEBIT_CARD, "10.00"));
+        expenses.add(new Expense("OpenShift for Developers, Second Edition",
+        Expense.PaymentMethod.DEBIT_CARD, "15.00"));
+    }
+
+    private Set<Expense> expenses = Collections.newSetFromMap(Collections.synchronizedMap(new HashMap<>()));
 
     public Set<Expense> list() {
         return expenses;
@@ -21,7 +30,7 @@ public class ExpenseService {
         return expense;
     }
 
-   public boolean delete(UUID uuid) {
+    public boolean delete(UUID uuid) {
         return expenses.removeIf(expense -> expense.getUuid().equals(uuid));
     }
 
